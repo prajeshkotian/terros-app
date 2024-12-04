93aadb75382aa59a183aed42297b8bc591ba2871
Prajesh Kotian <prajeshkotian@gmail.com>
2024-12-04 15:25:49 -0500

    Added color to chess pieces and set chess pieces to individual players, Also added moveemnt to pieces on board

--- src/Chess Components/Chessboard.jsx
+++ src/Chess Components/Chessboard.jsx
@@ -1,26 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Chessboard.css'
 
+import Square from './Square'
+
+
+const initialBoard = [
+    [
+        { piece: '\u265C', player: 'player2' },
+        { piece: '\u265E', player: 'player2' }, 
+        { piece: '\u265D', player: 'player2' },
+        { piece: '\u265B', player: 'player2' },
+        { piece: '\u265A', player: 'player2' },
+        { piece: '\u265D', player: 'player2' },
+        { piece: '\u265E', player: 'player2' },
+        { piece: '\u265C', player: 'player2' },
+      ],
+      [
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+        { piece: '\u265F', player: 'player2' },
+      ],
+      [{ piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }],
+      [{ piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }],
+      [{ piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }],
+      [{ piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }, { piece: '', player: null }],
+      [
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+        { piece: '\u2659', player: 'player1' },
+      ],
+      [
+        { piece: '\u2656', player: 'player1' },
+        { piece: '\u2658', player: 'player1' },
+        { piece: '\u2657', player: 'player1' },
+        { piece: '\u2655', player: 'player1' },
+        { piece: '\u2654', player: 'player1' },
+        { piece: '\u2657', player: 'player1' },
+        { piece: '\u2658', player: 'player1' },
+        { piece: '\u2656', player: 'player1' },
+      ],
+  ];
+
 export default function Chessboard() {
-    const board = [];
-    for (let row = 0; row < 8; row++) {
-      const squares = [];
-      for (let col = 0; col < 8; col++) {
-        const isBlack = (row + col) % 2 === 1;
-        squares.push(
-          <div
-            key={`${row}-${col}`}
-            className={`square ${isBlack ? 'black' : 'white'}`}
-          ></div>
-        );
-      }
-      console.log(squares)
-      board.push(
-        <div key={row} className="row">
-          {squares.map(square=> square)}
+
+    const [boardState, setBoardState] = useState(initialBoard)
+    const [selectedPiece, setSeletedPiece] = useState(null)
+
+    const onPieceSelect=(row, col)=>{
+        if(!selectedPiece){ //if piece is not selected set the selcted piece
+            const obj={
+                ...boardState[row][col],
+                row: row,
+                col: col
+            }
+            setSeletedPiece(obj)
+        }else{ // if piece is already selected we are moving to new position on board
+            const newBoard=[...boardState]
+            newBoard[row][col]={...boardState[selectedPiece?.row][selectedPiece?.col]}
+            newBoard[selectedPiece?.row][selectedPiece?.col]={ piece: '', player: null }
+            setSeletedPiece(null)
+            setBoardState(newBoard)
+        }
+        
+    }
+
+
+    return (
+        <div className="chessboard">
+          {boardState.map((row, rowIndex) => (
+            <div key={rowIndex} className="row">
+              {row.map((square, colIndex) => (
+                <Square key={`${rowIndex}-${colIndex}`} piece={square?.piece} isBlack={(rowIndex+colIndex) % 2 === 1 ? true : false} player={square?.player} row={rowIndex} col={colIndex} onPieceSelect={onPieceSelect} selectedPiece={selectedPiece}/>
+              ))}
+            </div>
+          ))}
         </div>
       );
-    }
-  
-    return (<div className="chessboard">{board}</div>);
 }
+  
--- src/Chess Components/Square.jsx
+++ src/Chess Components/Square.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import './Chessboard.css'
 
-export default function Square({piece, ...props}) {
+export default function Square({piece, isBlack, player, row, col, onPieceSelect, selectedPiece, ...props}) {
   return (
-    <div className='square'>
-      {piece}
+    <div className={`square ${isBlack ? 'black' : 'white'}`} onClick={()=>onPieceSelect(row, col)}>
+        <span className={`${
+        player === 'player1'
+          ? 'player1'
+          : player === 'player2'
+          ? 'player2'
+          : ''
+      } ${selectedPiece && selectedPiece.row === row && selectedPiece.col === col ? 'Selected' : ''}`}>
+          {piece}
+        </span>
     </div>
   )
 }
